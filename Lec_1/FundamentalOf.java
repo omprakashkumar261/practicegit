d5353a440ff7cd0eafd8fc94d6286a35f81ab817
ompralash kumar <omprakshkumar261@gmail.com>
2024-03-03 14:26:02 +0530

    this is my first commit

---
+++ Lec_1/FundamentalOf.java
@@ -0,0 +1,11 @@
+public class FundamentalOf{
+    public static void main(String[] args) {
+        System.out.println(" Hello java welcome to in this class ! ");
+        for(int i=1;i<5;i++){
+            System.out.println(" i = "+i);
+           
+        }
+    }
+    
+}
+ 
