7528821b992e1219dd959d933aad92f2c4db233f
omprakash kumar <npoint443@gmail.com>
2024-03-03 15:25:05 +0530

    this is my second commint

--- Lec_1/FundamentalOf.java
+++ Lec_1/FundamentalOf.java
@@ -1,6 +1,6 @@
 public class FundamentalOf{
     public static void main(String[] args) {
-        System.out.println(" Hello java welcome to in this class ! ");
+        System.out.println(" Hello java welcome to in this class g! ");
         for(int i=1;i<5;i++){
             System.out.println(" i = "+i);
            
---
+++ Lec_1/How.java
@@ -0,0 +1,5 @@
+public class How {
+    public  static void main(String []args){
+        System.out.println("hello !");
+    }
+}
