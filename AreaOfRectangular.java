14d8f8e5c03a1f4cd27627edcc4ea1f0285b2a41
omprakashkumar261 <161987926+omprakashkumar261@users.noreply.github.com>
2024-03-09 15:04:53 +0530

    This is a new problem day1

---
+++ AreaOfRectangular.java
@@ -0,0 +1,13 @@
+package PeerProblem;
+
+import java.util.Scanner;
+
+public class AreaOfRectangular {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int l = sc.nextInt();
+       int b = sc.nextInt();
+       int area = l*b;
+       System.out.println(area);
+    }
+}
